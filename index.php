25d5b96e34b8d0b8e9d49254a17e081319e6d18d
thearck <the_arck@yahoo.co.id>
2016-02-02 11:49:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php include "header.php"; ?>
+
+</body>
+<?php include "menu.php"; ?>
+<body>
+<?php include "home.php"; ?>
+</body>
+<?php include "footer.php"; ?>
+</html>
+<?php
+//@2016
+//thearck 199002082010121003
+//febrian.gilang@gmail.com
+//semoga bermanfaat
+?>
